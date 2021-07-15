64cba04313ce570e1ea88073f0198450ba107612
matias geber <matiasgeber74@gmail.com>
2021-07-15 14:53:08 -0300

    subiendo proyecto nuevo

---
+++ Tp-promocion/src/main/java/ar/edu/unlp/info/bd2/Tp/promocion/TpPromocionApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.bd2.Tp.promocion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPromocionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpPromocionApplication.class, args);
+	}
+
+}
---
+++ Tp-promocion/src/test/java/ar/edu/unlp/info/bd2/Tp/promocion/TpPromocionApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlp.info.bd2.Tp.promocion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPromocionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
